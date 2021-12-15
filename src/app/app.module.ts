e346a0a6fd9034fe86e6b9cd19321742699d2932
ESCUDERO, VICTORIA <victoriaescudero_99@hotmail.com>
2021-12-15 09:20:37 -0300

    Rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { RegisterComponent } from './components/register/register.component';
-import { EditUserComponent } from './components/editUser/editUser.component';
-import { UsuarioserviceService } from './services/usuarioservice.service';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './components/homefoodtrucker/homefoodtrucker.component';
-import { HomeServicioComponent } from './components/homeorganizador/homeorganizador.component';
-import { NewfServicioComponent } from './components/newfoodtruck/newfoodtruck.component';
-import { FoodtruckService } from './services/foodtruck.service';
-import { ListServiciosComponent } from './components/listfoodtrucks/listfoodtrucks.component';
-import { BorrarServicioComponent } from './components/borrarServicio/borrarServicio.component';
-import { EditServicioComponent } from './components/edit-food-truck/edit-food-truck.component';
 import { HomeComponent } from './components/home/home.component';
 import { HomeServiciosComponent } from './components/home-servicios/home-servicios.component';
-import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { NewServicioComponent } from './components/new-servicio/new-servicio.component';
 import { ListServiciosComponent } from './components/list-servicios/list-servicios.component';
+import { BorrarServicioComponent } from './components/borrar-servicio/borrar-servicio.component';
+import { EditServicioComponent } from './components/edit-servicio/edit-servicio.component';
+
 
 
 @NgModule({
@@ -26,11 +21,11 @@ import { ListServiciosComponent } from './components/list-servicios/list-servici
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    EdituserComponent,
-    HomeServicioComponent,
-    HomeServicioComponent,
+    EditUserComponent,
+    HomeComponent,
+    HomeServiciosComponent,
     NewServicioComponent,
-    ListServicioComponent,
+    ListServiciosComponent,
     BorrarServicioComponent,
     EditServicioComponent,
     HomeComponent,
@@ -39,7 +34,7 @@ import { ListServiciosComponent } from './components/list-servicios/list-servici
     ListServiciosComponent
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  providers: [UsuarioserviceService, FoodtruckService],
+  //providers: [UsuarioserviceService, FoodtruckService], (Ver para que es esto)
   bootstrap: [AppComponent],
 })
 export class AppModule {}
